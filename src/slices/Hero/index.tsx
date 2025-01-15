915d22246e031b51bc2ad496fd6ae1613db1b620
Sérgio Correia <sergiosdcorreia@gmail.com>
2025-01-15 18:27:25 +0000

    Hero section and project structure.

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,29 @@
+import { Content } from "@prismicio/client";
+import { PrismicNextLink } from "@prismicio/next";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { JSX } from "react";
+
+import { Bounded } from "@/components/Bounded";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero = ({ slice }: HeroProps): JSX.Element => {
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <PrismicRichText field={slice.primary.heading} />
+      <PrismicRichText field={slice.primary.body} />
+      <PrismicNextLink field={slice.primary.button} />
+    </Bounded>
+  );
+};
+
+export default Hero;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,15 @@
 import type { Config } from "tailwindcss";
+import fluid, { extract } from 'fluid-tailwind'
 
 export default {
-  content: [
+  content: {files:[
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/slices/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  extract
+},
   theme: {
     extend: {
       colors: {
@@ -14,5 +18,7 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    fluid
+  ],
 } satisfies Config;
