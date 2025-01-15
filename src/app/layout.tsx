cd8116239d3e30d9459693913f27d9bf7145c387
Sérgio Correia <sergiosdcorreia@gmail.com>
2025-01-15 20:48:04 +0000

    Adding header with nav, logo and cart button.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Bowlby_One_SC, DM_Mono } from "next/font/google";
 import "./globals.css";
+import { Header } from "@/components/Header";
 
 const bowlby = Bowlby_One_SC({
   subsets: ['latin'],
@@ -31,7 +32,10 @@ export default function RootLayout({
       <body
         className={`${bowlby.variable} ${dmMono.variable} antialiased font-mono font-medium text-zinc-800`}
       >
-        {children}
+        <main>
+          <Header />
+          {children}
+        </main>
       </body>
     </html>
   );
