97f2a8585340a544771cb3cccb0d004d18637f6f
Sérgio Correia <sergiosdcorreia@gmail.com>
2025-01-20 22:12:31 +0000

    Building the stacked sections - making them stack on top of each other with display sticky and dynamic position based on index.

--- src/slices/TextAndImage/index.tsx
+++ src/slices/TextAndImage/index.tsx
@@ -4,8 +4,14 @@ import { Heading } from "@/components/Heading";
 import { Content } from "@prismicio/client";
 import { PrismicRichText, PrismicText, SliceComponentProps } from "@prismicio/react";
 import clsx from "clsx";
-import { JSX } from "react";
+import { JSX } from 'react';
 import ParallaxImage from "./ParallaxImage";
+
+declare module "react" {
+  interface CSSProperties {
+    "--index"?: number
+  }
+}
 
 /**
  * Props for `TextAndImage`.
@@ -15,7 +21,7 @@ export type TextAndImageProps = SliceComponentProps<Content.TextAndImageSlice>;
 /**
  * Component for "TextAndImage" Slices.
  */
-const TextAndImage = ({ slice }: TextAndImageProps): JSX.Element => {
+const TextAndImage = ({ slice, index }: TextAndImageProps): JSX.Element => {
 const theme = slice.primary.theme
 
   return (
@@ -23,11 +29,13 @@ const theme = slice.primary.theme
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       className={clsx(
+        "sticky top-[calc(var(--index)*2rem)]",
         theme === "Blue" && "bg-texture bg-brand-blue text-white",
         theme === "Orange" && "bg-texture bg-brand-orange text-white",
         theme === "Navy" && "bg-texture bg-brand-navy text-white",
         theme === "Lime" && "bg-texture bg-brand-lime",
       )}
+      style={{ "--index": index}}
     >
       <div className="grid grid-cols-1 items-center gap-12 md:grid-cols-2 md:gap-24">
         <div className={clsx(
