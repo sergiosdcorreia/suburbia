91eb3c92ab3215e39bbe1f2313d396222310f0f2
Sérgio Correia <sergiosdcorreia@gmail.com>
2025-01-21 16:37:14 +0000

    Adding team section with scale animations and scribbles.

---
+++ src/slices/TeamGrid/index.tsx
@@ -0,0 +1,46 @@
+import { Bounded } from "@/components/Bounded";
+import { Heading } from "@/components/Heading";
+import { createClient } from "@/prismicio";
+import { Content } from "@prismicio/client";
+import { PrismicText, SliceComponentProps } from "@prismicio/react";
+import React, { JSX } from "react";
+import { Skater } from "./Skater";
+
+/**
+ * Props for `TeamGrid`.
+ */
+export type TeamGridProps = SliceComponentProps<Content.TeamGridSlice>;
+
+/**
+ * Component for "TeamGrid" Slices.
+ */
+
+const TeamGrid = async ({ slice }: TeamGridProps): Promise<JSX.Element> => {
+  const client = createClient()
+  const skaters = await client.getAllByType("skater")
+
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className="bg-texture bg-brand-navy"
+    >
+      <Heading as="h2" size="lg" className="mb-8 text-center text-white">
+        <PrismicText field={slice.primary.heading} />
+      </Heading>
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-4">
+        {skaters.map((skater, index) => (
+          <React.Fragment key={index}>
+            {
+              skater.data.firstname && (
+                <Skater index={index} skater={skater} />
+              )
+            }
+          </React.Fragment>
+        ))}
+      </div>
+    </Bounded>
+  );
+};
+
+export default TeamGrid;
