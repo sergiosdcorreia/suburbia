d3c2dc129cc6834f3fbf1e442fd8d9327fab9bd5
Sérgio Correia <sergiosdcorreia@gmail.com>
2025-01-21 23:10:19 +0000

    Adding physics to boards in the footer section.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,15 @@
 import { createClient } from '@/prismicio'
 import { PrismicNextImage, PrismicNextLink } from '@prismicio/next'
-import React from 'react'
 import { Logo } from '@/components/Logo'
 import { Bounded } from './Bounded'
+import { FooterPhysics } from './FooterPhysics'
+import { asImageSrc } from '@prismicio/client'
 
 export async function Footer() {
   const client = createClient()
   const settings = await client.getSingle("settings")
+
+  const boardTextureUrls = settings.data.footer_skateboards.map((item) => asImageSrc(item.skateboard, {h: 600})).filter((url):url is string => Boolean(url))
 
   return (
     <footer className='bg-texture bg-zinc-900 text-white overflow-hidden'>
@@ -14,6 +17,7 @@ export async function Footer() {
         {/* Image */}
         <PrismicNextImage field={settings.data.footer_image} alt="" fill className="object-cover" width={1200} />
         {/* Physics Boards */}
+        <FooterPhysics boardTextureURLs={boardTextureUrls} className='absolute inset-0 overflow-hidden' />
         {/* Logo */}
         <Logo className='pointer-events-none relative h-20 mix-blend-exclusion md:h-28' />
       </div>
